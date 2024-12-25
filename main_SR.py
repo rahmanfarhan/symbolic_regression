0a9dcd72920a955d7173db5a8ab50460e9502648
Farhan Rahman <alpacahelps@gmail.com>
2024-12-25 10:05:33 -0700

    added test data and basic calls

--- main_SR.py
+++ main_SR.py
@@ -1,1 +1,22 @@
-#create training data
+import numpy as np
+import matplotlib.pyplot as plt
+from bingo.symbolic_regression.symbolic_regressor import SymbolicRegressor
+
+X_0 = np.linspace(-10, 10, num=30).reshape((-1, 1))
+X = np.array(X_0)
+y = 5.0 * X_0 ** 2 + 3.5 * X_0
+
+plt.scatter(X, y)
+plt.xlabel("X_0")
+plt.ylabel("y")
+plt.title("Training Data")
+plt.show()
+
+regressor = SymbolicRegressor(population_size=500, stack_size=20,
+                              use_simplification=True)
+
+regressor.fit(X, y)
+
+best_individual = regressor.get_best_individual()
+print("best individual is:", best_individual)
+
