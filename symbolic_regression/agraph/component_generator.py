f9d2a2ae73f4b99bb3175b571e51b5a29eaf092d
Farhan Rahman <alpacahelps@gmail.com>
2024-12-29 17:01:16 -0700

    ComponentGenerator class working

--- symbolic_regression/agraph/component_generator.py
+++ symbolic_regression/agraph/component_generator.py
@@ -1,12 +1,14 @@
 
-
+import numpy as np
 from util.probability_mass_function import ProbabilityMassFunction
+from .operator_definitions import OPERATOR_NAMES
 
 class ComponentGenerator:
 
     def __init__(self, 
         input_x_dimension,
         num_initial_load_statements=1, 
+        terminal_probability=0.1,
         constant_probability=None):
 
         self.input_x_dimension = input_x_dimension
@@ -15,6 +17,24 @@ class ComponentGenerator:
         self._operator_pmf = ProbabilityMassFunction()
         self._terminal_pmf = self._make_terminal_pdf(constant_probability)
 
+        self._random_command_function_pmf = self._make_random_command_pmf(terminal_probability)
+
+    def _make_random_command_pmf(self, terminal_probability):
+        command_weights = [terminal_probability,
+                           1.0 - terminal_probability]
+        return ProbabilityMassFunction(items=[self.random_terminal_command,
+                                              self.random_operator_command],
+                                       weights=command_weights)
+
+    def random_operator_command(self, stack_location):
+
+        return np.array([self.random_operator(),
+                         self.random_operator_parameter(stack_location),
+                         self.random_operator_parameter(stack_location)],
+                        dtype=int)
+
+    def random_operator(self):
+        return self._operator_pmf.draw_sample()
 
     def _make_terminal_pdf(self, constant_probability):
         if constant_probability is None:
@@ -22,14 +42,37 @@ class ComponentGenerator:
         else:
             terminal_weight = [constant_probability,
                                1.0 - constant_probability]
-                               
+
         return ProbabilityMassFunction(items=[1, 0], weights=terminal_weight)
+
+    
+    def random_terminal_parameter(self, terminal_number):
+        if terminal_number == 0:
+            param = np.random.randint(self.input_x_dimension)
+        else:
+            param = -1
+        return param
 
 
     def add_operator(self, operator_to_add, operator_weight=None):
-        operator_number = operator_to_add
+   
+        if isinstance(operator_to_add, str):
+            operator_number = self._get_operator_number_from_string(
+                operator_to_add)
+        else:
+            operator_number = operator_to_add
+
         self._operator_pmf.add_item(operator_number, operator_weight)
 
+    @staticmethod
+    def _get_operator_number_from_string(operator_string):
+        for operator_number, operator_names in OPERATOR_NAMES.items():
+            if operator_string in operator_names:
+                return operator_number
+        raise ValueError(f"Could not find operator {operator_string}. ")
+
+
+    @staticmethod
     def random_operator_parameter(stack_location):
         return np.random.randint(stack_location)    
 
--- util/probability_mass_function.py
+++ util/probability_mass_function.py
@@ -33,8 +33,47 @@ class ProbabilityMassFunction:
         if items is None:
             items = []
         self.items = items
-        self.weights = weights
 
+        if weights is None:
+            weights = self._get_default_weights()
+
+        self._is_weights_same_size_as_items(weights)
+        self._total_weight, self.normalized_weights = self._normalize_weights(weights)
+        self._cumulative_weights = np.cumsum(self.normalized_weights)
+
+    def _is_weights_same_size_as_items(self, weights):
+        if len(weights) != len(self.items):
+            LOGGER.error("Initialization of ProbabilityMassFunction with "
+                         "items and weights of different dimensions")
+            LOGGER.error("items = %s", self.items)
+            LOGGER.error("weights = %s", weights)
+            raise ValueError
+
+    @staticmethod
+    def _normalize_weights(weights):
+        total_weight = np.sum(weights)
+        normalized_weights = np.array(weights) / total_weight
+        ProbabilityMassFunction._check_valid_weights(normalized_weights,
+                                                     weights)
+        return total_weight, normalized_weights
+
+    @staticmethod
+    def _check_valid_weights(normalized_weights, weights):
+        if normalized_weights.size > 0:
+            if not np.isclose(np.sum(normalized_weights), 1.0) or \
+                            np.min(normalized_weights) < 0.0:
+                LOGGER.error("Invalid weights encountered in "
+                             "ProbabilityMassFunction")
+                LOGGER.error("weights = %s", weights)
+                raise ValueError
+
+    def _get_default_weights(self):
+        n_items = len(self.items)
+        if n_items > 0:
+            weights = np.full(n_items, 1.0 / n_items)
+        else:
+            weights = np.array([])
+        return weights
  
     def add_item(self, new_item, new_weight=None):
         """Adds a single item to the PMF.
